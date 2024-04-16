ef419bb804a3f37ab5ebfc21587a0d77bd188e06
Space1230 <sandylovesapples@yahoo.com>
2024-04-16 11:58:44 -0400

    added ability to load camera info from an xml file

--- ros_start.py
+++ ros_start.py
@@ -22,6 +22,13 @@ from cv_bridge import CvBridge
 import cv2
 import time
 
+# xml parsing stuff
+import glob
+import xml.etree.ElementTree as ET
+
+python_script_directory = os.path.dirname(os.path.abspath(__file__)) + '/'
+
+## ROS Things
 def ros_preloop():
     rospy.init_node('publisher', anonymous=True, disable_signals=True)
 
@@ -29,9 +36,11 @@ def ros_postloop():
     rospy.signal_shutdown('CLI is done')
 
 def crab():
-    crab_location = os.path.dirname(os.path.abspath(__file__)) + '/crabs.mp4'
+    crab_location = python_script_directory + 'crabs.mp4'
     crabs = cv2.VideoCapture(crab_location);
     crab_publisher = rospy.Publisher('crab', Image, queue_size=10)
+    camera_info_publisher = rospy.Publisher('camera_info', CameraInfo, queue_size=10)
+    camera_info = get_camera_info()
     crab_bridge = CvBridge()
     print("CRAB CRAB CRAB!")
     try:
@@ -44,6 +53,7 @@ def crab():
 
             crab_msg = crab_bridge.cv2_to_imgmsg(crab_frame)
             crab_publisher.publish(crab_msg)
+            camera_info_publisher.publish(camera_info)
             time.sleep(1/60)
 
     except KeyboardInterrupt:
@@ -52,12 +62,37 @@ def crab():
         print("Crab stream done :(")
         print("")
 
+def xml_findall_to_item_list(element_list):
+    item_list = []
+    for element in element_list:
+        item_list.append(float(element.text))
+    return item_list
+
+def get_camera_info():
+    # grab an xml file
+    camera_info_files = glob.glob(python_script_directory + 'CameraInfo/*.xml')
+    if len(camera_info_files) == 0:
+        print("Please put your calibration data in the CameraInfo directory in the scripts folder")
+        return None
+    xml_file_path = camera_info_files[0]
+    tree = ET.parse(xml_file_path)
+    camera_info = CameraInfo()
+    camera_info.height = int(tree.find('Height').text)
+    camera_info.width = int(tree.find('Width').text)
+    camera_info.distortion_model = tree.find('DistortionModel').text
+    camera_info.D = xml_findall_to_item_list(tree.findall('D'))
+    camera_info.K = xml_findall_to_item_list(tree.findall('K'))
+    camera_info.R = xml_findall_to_item_list(tree.findall('R'))
+    camera_info.P = xml_findall_to_item_list(tree.findall('P'))
+    return camera_info
+
 def stream_amp_and_dist_over_ROS():
     "Streams amplify image to 'video_frames'"
     from GCC_API import get_image # this is needed cause python is stupid for some reason
     amp_publisher = rospy.Publisher('amp_frames', Image, queue_size=10)
     dist_publisher = rospy.Publisher('dist_frames', Image, queue_size=10)
-    camera_info = CameraInfo(height=240, width=360)
+    camera_info_publisher = rospy.Publisher('camera_info', CameraInfo, queue_size=10)
+    camera_info = get_camera_info()
     bridge = CvBridge()
     try:
         while not rospy.is_shutdown():
@@ -78,6 +113,7 @@ def stream_amp_and_dist_over_ROS():
 
             amp_publisher.publish(amp_msg)
             dist_publisher.publish(dist_msg)
+            camera_info_publisher.publish(camera_info)
 
     except KeyboardInterrupt:
         print("ROS stream finished")
